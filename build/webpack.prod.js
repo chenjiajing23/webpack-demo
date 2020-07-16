984558e01c2e34704d11ae4e7c892d9e6a4ad5e4
陈家敬 <chenjiajing@forchange.tech>
2020-07-16 22:41:22 +0800

    feat(master): 压缩图片

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -29,36 +29,36 @@ module.exports = merge(common, {
 
   module: {
     rules: [
-      ...utils.styleLoaders(true)
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      //   use: [
-      //     {
-      //       loader: 'image-webpack-loader',
-      //       options: {
-      //         mozjpeg: {
-      //           progressive: true,
-      //           quality: 65
-      //         },
-      //         optipng: {
-      //           enabled: false
-      //         },
-      //         pngquant: {
-      //           quality: [0.65, 0.9],
-      //           speed: 4
-      //         },
-      //         gifsicle: {
-      //           interlaced: false
-      //         },
-      //         webp: {
-      //           quality: 75
-      //         }
-      //       }
-      //     }
-      //   ],
-      //   include: path.resolve(__dirname, '../src'),
-      //   exclude: /node_modules/
-      // }
+      ...utils.styleLoaders(true),
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        include: path.resolve(__dirname, '../src'),
+        exclude: /node_modules/
+      }
     ]
   },
 
