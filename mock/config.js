f7b8381476d87b04e7f6d9c9daf8af45fad4b11c
陈家敬 <chenjiajing@forchange.tech>
2020-09-20 17:31:11 +0800

    feat: 🎸 master
    update

---
+++ mock/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: Mock配置
+ * @Author: Chenjiajing
+ * @Date: 2020-07-15 22:48:38
+ * @LastEditors: Chenjiajing
+ * @LastEditTime: 2020-09-20 17:28:26
+ */
+
+module.exports = {
+  host: 'localhost',
+  port: 1234,
+  https: false
+}
