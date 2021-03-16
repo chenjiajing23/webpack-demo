8b2978daf99737629edc1eac6a6ecad35d71abb0
陈家敬 <16602132375@163.com>
2021-03-16 23:20:38 +0800

    feat: 🎸 canvas-test
    canvas入门

--- src/modules/canvas/pages/CanvasTest.tsx
+++ src/modules/canvas/pages/CanvasTest.tsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import '../style/canvas-test.less';
 import imgSrc from '../assets/test.png';
+import { Button } from 'antd';
 
 interface IPoint {
   x: number;
@@ -9,17 +10,25 @@ interface IPoint {
 }
 
 const CanvasTest = () => {
+  const imgRef = useRef<HTMLImageElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const startPoint = useRef<IPoint>({ x: 0, y: 0 });
+  const [isMoving, setMoving] = useState(false); // 是否在移动
 
+  // 画图
+  const printImg = (canvas: HTMLCanvasElement, img: HTMLImageElement, ctx: CanvasRenderingContext2D) => {
+    const xRate = canvas.width / img.width;
+    const yRate = canvas.height / img.height;
+    ctx.drawImage(img, 0, 0, img.width * xRate, img.height * yRate);
+  };
+
+  // 图片加载成功
   const onLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const node = canvasRef.current;
     const ctx = node?.getContext('2d');
     if (node && ctx) {
       const img = e.target as HTMLImageElement;
-      node.width = img.width;
-      node.height = img.height;
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+      printImg(node, img, ctx);
     }
   };
 
@@ -50,9 +59,36 @@ const CanvasTest = () => {
     };
   };
 
+  const drawRect = (ctx: CanvasRenderingContext2D, curPoint: { x: number; y: number }) => {
+    const newRect = getRectParam(curPoint, startPoint.current);
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'rgba(200, 0, 0, 0.5)';
+    ctx.rect(newRect.startPoint.x, newRect.startPoint.y, Math.abs(newRect.w), Math.abs(newRect.h));
+    ctx.stroke();
+  };
+
+  // 导出图片
+  const exportImage = () => {
+    const node = canvasRef.current as HTMLCanvasElement;
+    const dataURL = node.toDataURL('image/png', 1);
+
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.download = 'canvas-test';
+    a.href = dataURL;
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+  };
+
+  // 按下鼠标
   const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    setMoving(true);
     const node = canvasRef.current as HTMLCanvasElement;
     const ctx = node.getContext('2d') as CanvasRenderingContext2D;
+
     const x = e.pageX;
     const y = e.pageY;
     startPoint.current = getPointOnCanvas(node, x, y); // 获取起点
@@ -60,29 +96,48 @@ const CanvasTest = () => {
     ctx.moveTo(startPoint.current.x, startPoint.current.y);
   };
 
+  // 移动鼠标
   const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (!isMoving) {
+      return;
+    }
     const node = canvasRef.current as HTMLCanvasElement;
     const ctx = node.getContext('2d') as CanvasRenderingContext2D;
+
+    // 清除画布
+    ctx.clearRect(0, 0, node.width, node.height);
+    printImg(node, imgRef.current as HTMLImageElement, ctx);
+
     const x = e.pageX;
     const y = e.pageY;
     const curPoint = getPointOnCanvas(node, x, y);
     drawRect(ctx, curPoint);
   };
 
-  const drawRect = (ctx: CanvasRenderingContext2D, curPoint: { x: number; y: number }) => {
-    const newRect = getRectParam(curPoint, startPoint.current);
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgba(200, 0, 0, 0.5)';
-    ctx.rect(newRect.startPoint.x, newRect.startPoint.y, Math.abs(newRect.w), Math.abs(newRect.h));
-    ctx.stroke();
+  // 松开鼠标
+  const onMouseUp = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    setMoving(false);
+    console.log(e.pageX, e.pageY);
   };
 
   return (
     <section styleName="canvas-test">
       <div className="content">
-        <img className="image" src={imgSrc} width="800px" alt="" onLoad={onLoad} />
-        <canvas className="canvas" ref={canvasRef} onMouseMove={onMouseMove} onMouseDown={onMouseDown} />
+        <img ref={imgRef} className="image" src={imgSrc} alt="" onLoad={onLoad} />
+        <canvas
+          width={800}
+          height={500}
+          className="canvas"
+          ref={canvasRef}
+          onMouseMove={onMouseMove}
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+        />
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <Button type="primary" onClick={exportImage}>
+          导出图片
+        </Button>
       </div>
     </section>
   );
